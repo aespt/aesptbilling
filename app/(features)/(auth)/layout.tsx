08eb1a8c0899c19e83357793a3ddc149cca1856e
Anuj <anuj@Anujs-MacBook-Air.local>
2025-05-04 22:17:35 +0530

    fix(app): access inner pages without authentication

--- app/(features)/(auth)/layout.tsx
+++ app/(features)/(auth)/layout.tsx
@@ -2,13 +2,16 @@
 
 import Header from '@/app/shared/components/header';
 import Sidebar from '@/app/shared/components/sidebar';
+import { AuthGuard } from '@/app/components/AuthGuard';
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen bg-gray-50 pt-6">
-      <Sidebar />
-      <Header />
-      <main>{children}</main>
-    </div>
+    <AuthGuard>
+      <div className="min-h-screen bg-gray-50 pt-6">
+        <Sidebar />
+        <Header />
+        <main>{children}</main>
+      </div>
+    </AuthGuard>
   );
 }
